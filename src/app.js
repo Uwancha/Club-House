4998210ac967677b3a0b2085803e4fe23a5613d8
Uwancha <sito2alemu@gmail.com>
2023-12-28 22:28:25 +0300

    feat: Setup 404 and error handler middlewares

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
-//import pug from "pug"
+import createError from "http-errors";
 
 configDotenv();
 
@@ -59,7 +59,23 @@ const limiter = rateLimit({
 
 app.use(limiter)
 
-app.get('/', (req, res) => res.render("index"))
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 
 
 const port = process.env.PORT || 3000
