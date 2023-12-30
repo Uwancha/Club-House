9babe63b3d8c514434031754b6ce05347dec8e09
Uwancha <sito2alemu@gmail.com>
2023-12-31 01:58:58 +0300

    feat: Setup logging out and joining club routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,13 @@
 import express from "express";
 
-import { getSignupForm, signUpUser, getLoginForm, handleLoggingIn } from "../controllers/userController.js"
+import { 
+    getSignupForm, 
+    signUpUser, 
+    getLoginForm,
+    handleLoggingIn,
+    getJoinForm,
+    joinUser
+ } from "../controllers/userController.js";
 
 const router = express.Router();
 
@@ -8,9 +15,23 @@ const router = express.Router();
 router.get("/signup", getSignupForm);
 router.post("/signup", signUpUser);
 
-
 // Login routes
 router.get("/login", getLoginForm);
 router.post("/login", handleLoggingIn)
+
+// Logout user
+router.get("/logout", (req, res, next) => {
+    req.logOut(err => {
+        if (err) {
+            return next(err)
+        }
+
+        res.redirect('/')
+    })
+})
+
+// Let user join club
+router.get("/join", getJoinForm)
+router.post("/join", joinUser)
 
 export { router as userRouter };
